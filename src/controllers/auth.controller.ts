dd89750de867a3697bb70f115561a0d81eb15bd2
ix1osbek <bekerkinov2004@gmail.com>
2025-05-17 00:32:29 +0500

    get_one_admin function added

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -65,6 +65,27 @@ export const getAllAdmins = async (req: Request, res: Response) => {
     res.status(500).json({ message: 'Server xatosi.' });
   }
 };
+
+
+///////////////////// get admin by id
+export const getAdminById = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+
+    const admin = await User.findByPk(id, {
+      attributes: ['id', 'login', 'role'],
+    });
+
+    if (!admin) {
+      return res.status(404).json({ message: 'Admin topilmadi' });
+    }
+
+    res.status(200).json(admin);
+  } catch (error) {
+    console.error('getAdminById xatolik:', error);
+    res.status(500).json({ message: 'Server xatosi.' });
+  }
+};
 //////////////// login 
 
 
--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,6 +1,6 @@
 
 import { RequestHandler, Router } from 'express';
-import { createAdmin, deleteAdmin, getAllAdmins, login } from '../controllers/auth.controller';
+import { createAdmin, deleteAdmin, getAdminById, getAllAdmins, login } from '../controllers/auth.controller';
 import { checkRole } from '../middlewares/roleMiddleware';
 import { authenticate } from '../middlewares/authMiddleware';
 
@@ -8,6 +8,7 @@ const authRouter: Router = Router()
 authRouter.post('/create_admin',authenticate as RequestHandler, checkRole("superadmin") as RequestHandler, createAdmin as RequestHandler)
 authRouter.delete('/delete_admin/:id',authenticate as RequestHandler, checkRole("superadmin") as RequestHandler, deleteAdmin as RequestHandler)
 authRouter.get('/all_admins',authenticate as RequestHandler, checkRole("superadmin") as RequestHandler, getAllAdmins as RequestHandler)
+authRouter.get('/admin/:id',authenticate as RequestHandler, checkRole("superadmin") as RequestHandler, getAdminById as RequestHandler)
 authRouter.post("/login", login as RequestHandler)
 
 export default authRouter
