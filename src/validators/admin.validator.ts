76baea1bd7337e8f6f8f9c90746dc437fe42b3a4
ix1osbek <bekerkinov2004@gmail.com>
2025-05-17 17:57:29 +0500

    JOI validator improved

--- src/validators/admin.validator.ts
+++ src/validators/admin.validator.ts
@@ -18,8 +18,20 @@ export const createAdminSchema = Joi.object({
     })
 })
 export const updateAdminSchema = Joi.object({
-  login: Joi.string().alphanum().min(3).max(30).optional(),
-  password: Joi.string().min(6).optional()
+  login: Joi.string().alphanum().min(3).max(30).optional().messages({
+    'string.empty': 'Login bo‘sh bo‘lmasligi kerak',
+    'string.min': 'Login kamida 3 ta belgidan iborat bo‘lishi kerak',
+    'string.max': 'Login 30 belgidan oshmasligi kerak',
+    'any.required': 'Login kiritilishi shart',
+    'string.base': 'Login string bo‘lishi kerak',
+    'string.alphanum': 'Login faqat harf va raqamlardan iborat bo‘lishi kerak!'
+
+  }),
+  password: Joi.string().min(6).required().messages({
+    'string.min': 'Parol 6 ta belgidan kam bo‘lmasligi kerak',
+    'string.empty': 'Parol kiritilishi shart',
+    'any.required': 'Parol kiritilishi shart'
+  })
 }).or('login', 'password')
 
 
