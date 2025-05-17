09c139b852c1875a2deddebf36a0802fe33dd965
ix1osbek <bekerkinov2004@gmail.com>
2025-05-17 22:12:25 +0500

    client validated with joi

--- src/routes/client.routes.ts
+++ src/routes/client.routes.ts
@@ -3,9 +3,11 @@ import { Router, RequestHandler } from 'express'
 import { addClient } from '../controllers/clients.controller'
 import { authenticate } from "../middlewares/authMiddleware"
 import { checkRole } from "../middlewares/roleMiddleware"
+import { validateBody } from '../middlewares/validate'
+import { createClientSchema } from '../validators/client.validator'
 
 const clientRouter = Router()
 
-clientRouter.post('/clients/add_client', authenticate as RequestHandler, checkRole('admin', 'superadmin') as RequestHandler, addClient as RequestHandler)
+clientRouter.post('/clients/add_client', authenticate as RequestHandler, checkRole('admin', 'superadmin') as RequestHandler, validateBody(createClientSchema), addClient as RequestHandler)
 
 export default clientRouter
---
+++ src/validators/client.validator.ts
@@ -0,0 +1,24 @@
+import Joi from "joi";
+
+export const createClientSchema = Joi.object({
+    name: Joi.string().min(2).max(30).required()
+        .messages({
+            'string.empty': 'Ism bo‘sh bo‘lmasligi kerak',
+            'string.min': 'Ism kamida 2 ta belgidan iborat bo‘lishi kerak',
+            'string.max': 'Ism 30 belgidan oshmasligi kerak',
+            'any.required': 'Ism kiritilishi shart'
+        }),
+    surname: Joi.string().min(2).max(30).required()
+        .messages({
+            'string.empty': 'Familiya bo‘sh bo‘lmasligi kerak',
+            'string.min': 'Familiya kamida 2 ta belgidan iborat bo‘lishi kerak',
+            'string.max': 'Familiya 30 belgidan oshmasligi kerak',
+            'any.required': 'Familiya kiritilishi shart'
+        }),
+    phone: Joi.string().pattern(/^\+998\d{9}$/).required()
+        .messages({
+            'string.empty': 'Telefon raqami bo‘sh bo‘lmasligi kerak',
+            'string.pattern.base': 'Telefon raqami +998 bilan boshlanishi va 12 ta raqamdan iborat bo‘lishi kerak',
+            'any.required': 'Telefon raqami kiritilishi shart'
+        })
+})
